925703540907f06862894fea0390967ae74f26ac
Ferrancgg <ferrancgg@gmail.com>
2023-11-13 07:54:04 +0100

    primera parte acabada, falta configurar HTML para crearlo con DOM, falta empezar a aplicar filtros en DATA y mostrar resultados

---
+++ main.js
@@ -0,0 +1,215 @@
+const plantas = [
+  {
+    id: 1,
+    nombre: "Helecho Boston",
+    categoria: "Plantas de interior",
+    precioVenta: 12.99,
+    foto: "https://ejemplo.com/helecho.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Tomate",
+    categoria: "Plantas de exterior",
+    precioVenta: 2.49,
+    foto: "https://ejemplo.com/tomate.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Orquídea",
+    categoria: "Plantas de interior",
+    precioVenta: 15.99,
+    foto: "https://ejemplo.com/orquidea.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Suculenta Echeveria",
+    categoria: "Plantas de interior",
+    precioVenta: 8.99,
+    foto: "https://ejemplo.com/suculenta.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Geranio",
+    categoria: "Plantas de exterior",
+    precioVenta: 5.99,
+    foto: "https://ejemplo.com/geranio.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Ficus Lyrata",
+    categoria: "Plantas de interior",
+    precioVenta: 19.99,
+    foto: "https://ejemplo.com/ficus.jpg",
+  },
+  {
+    id: 7,
+    nombre: "Lavanda",
+    categoria: "Plantas de exterior",
+    precioVenta: 7.49,
+    foto: "https://ejemplo.com/lavanda.jpg",
+  },
+  {
+    id: 8,
+    nombre: "Cactus de Navidad",
+    categoria: "Plantas de interior",
+    precioVenta: 14.99,
+    foto: "https://ejemplo.com/cactus_navidad.jpg",
+  },
+  {
+    id: 9,
+    nombre: "Rosa",
+    categoria: "Plantas de exterior",
+    precioVenta: 10.99,
+    foto: "https://ejemplo.com/rosa.jpg",
+  },
+  {
+    id: 10,
+    nombre: "Potos",
+    categoria: "Plantas de interior",
+    precioVenta: 11.99,
+    foto: "https://ejemplo.com/potos.jpg",
+  },
+  {
+    id: 11,
+    nombre: "Cactus de barril",
+    categoria: "Plantas de interior",
+    precioVenta: 9.99,
+    foto: "https://ejemplo.com/cactus.jpg",
+  },
+  {
+    id: 12,
+    nombre: "Pimiento",
+    categoria: "Plantas de exterior",
+    precioVenta: 3.49,
+    foto: "https://ejemplo.com/pimiento.jpg",
+  },
+  {
+    id: 13,
+    nombre: "Crisantemo",
+    categoria: "Plantas de exterior",
+    precioVenta: 6.99,
+    foto: "https://ejemplo.com/crisantemo.jpg",
+  },
+  {
+    id: 14,
+    nombre: "Bambú de la suerte",
+    categoria: "Plantas de interior",
+    precioVenta: 7.99,
+    foto: "https://ejemplo.com/bambu.jpg",
+  },
+  {
+    id: 15,
+    nombre: "Aloe Vera",
+    categoria: "Plantas de interior",
+    precioVenta: 8.99,
+    foto: "https://ejemplo.com/aloe.jpg",
+  },
+  {
+    id: 16,
+    nombre: "Caléndula",
+    categoria: "Plantas de exterior",
+    precioVenta: 4.99,
+    foto: "https://ejemplo.com/calendula.jpg",
+  },
+  {
+    id: 17,
+    nombre: "Jade",
+    categoria: "Plantas de interior",
+    precioVenta: 11.49,
+    foto: "https://ejemplo.com/jade.jpg",
+  },
+  {
+    id: 18,
+    nombre: "Clivia",
+    categoria: "Plantas de interior",
+    precioVenta: 12.99,
+    foto: "https://ejemplo.com/clivia.jpg",
+  },
+  {
+    id: 19,
+    nombre: "Petunia",
+    categoria: "Plantas de exterior",
+    precioVenta: 5.49,
+    foto: "https://ejemplo.com/petunia.jpg",
+  },
+  {
+    id: 20,
+    nombre: "Cactus de cereus",
+    categoria: "Plantas de interior",
+    precioVenta: 10.99,
+    foto: "https://ejemplo.com/cactus_cereus.jpg",
+  },
+  {
+    id: 21,
+    nombre: "Ramo de Lilas Moradas",
+    categoria: "Ramos de Flores",
+    precioVenta: 26.99,
+    foto: "https://ejemplo.com/ramo_lilas.jpg",
+  },
+  {
+    id: 22,
+    nombre: "Ramo de Tulipanes Multicolores",
+    categoria: "Ramos de Flores",
+    precioVenta: 27.99,
+    foto: "https://ejemplo.com/ramo_tulipanes.jpg",
+  },
+  {
+    id: 23,
+    nombre: "Ramo de Girasoles",
+    categoria: "Ramos de Flores",
+    precioVenta: 21.99,
+    foto: "https://ejemplo.com/ramo_girasoles.jpg",
+  },
+  {
+    id: 24,
+    nombre: "Ramo de Lirios Amarillos",
+    categoria: "Ramos de Flores",
+    precioVenta: 25.99,
+    foto: "https://ejemplo.com/ramo_lirios_amarillos.jpg",
+  },
+  {
+    id: 25,
+    nombre: "Ramo de Margaritas Blancas",
+    categoria: "Ramos de Flores",
+    precioVenta: 19.99,
+    foto: "https://ejemplo.com/ramo_margaritas.jpg",
+  },
+];
+
+const product = document.querySelector("#product");
+const price=document.querySelector("#price")
+console.log(product.value);
+product.addEventListener("change", () => {
+  console.log(product.value);
+});
+
+const btt = document.querySelector("#btt");
+const resultProduct = document.querySelector("#result");
+const acumulador=[]
+const clean = () => {
+  resultProduct.innerHTML = ``;
+};
+const cambio = () => {
+  console.log(`soy${product.value}`);
+  clean();
+  console.log(price.value)
+
+  for (const planta of plantas) {
+    const htmlResult = `<h2>${planta.nombre}</h2>
+    <p>${planta.precioVenta}</p>`;
+
+    if (product.value === planta.categoria && price.value>planta.precioVenta) {
+      acumulador.push(planta)
+
+      resultProduct.innerHTML += htmlResult;
+    }
+
+  }
+  console.log(acumulador)
+  const htmlResultNone=`<h1>no tenemos resultados con esos criterio</h1>`
+  if (acumulador.length===0){
+
+    resultProduct.innerHTML=htmlResultNone
+  }
+};
+btt.addEventListener("click", cambio);
